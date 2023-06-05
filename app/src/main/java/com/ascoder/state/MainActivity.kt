4529356c4722151d4489375a9f5daed3478a73b5
Arfan Siregar <arfan.siregar@kpvendor.id>
2023-06-05 09:44:57 +0700

    - Define color as mutableStateOf with default value Color.Yellow and with remember as lazy

--- app/src/main/java/com/ascoder/state/MainActivity.kt
+++ app/src/main/java/com/ascoder/state/MainActivity.kt
@@ -7,6 +7,8 @@ import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,9 +29,13 @@ fun ComponentContainer() {
 
 @Composable
 fun ColorBox(modifier: Modifier = Modifier) {
+    val color by remember { lazy { mutableStateOf(Color.Yellow) } }
+
     Box(modifier = Modifier
-        .background(Color.Yellow)
-        .clickable {  }
+        .background(color = color.value)
+        .clickable {
+
+        }
     )
 }
 
