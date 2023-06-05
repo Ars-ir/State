fe72000ab15fdc0fe38a1e720b58dea3cfbba387
Arfan Siregar <arfan.siregar@kpvendor.id>
2023-06-05 09:33:18 +0700

    - Create box inner Color Box component

--- app/src/main/java/com/ascoder/state/MainActivity.kt
+++ app/src/main/java/com/ascoder/state/MainActivity.kt
@@ -3,8 +3,11 @@ package com.ascoder.state
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 
 class MainActivity : ComponentActivity() {
@@ -23,7 +26,7 @@ fun ComponentContainer() {
 
 @Composable
 fun ColorBox(modifier: Modifier = Modifier) {
-
+    Box()
 }
 
 @Preview(showBackground = true)
